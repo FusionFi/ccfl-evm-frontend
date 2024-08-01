b2c37ab80075f7c3377cd86d4c992d7e712120a7
dat <datvt@emurgo.africa>
2024-08-01 18:00:16 +0700

    add select component

--- pages/borrow/index.tsx
+++ pages/borrow/index.tsx
@@ -1,9 +1,16 @@
 import cssClass from '@/pages/borrow/index.module.scss';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { twMerge } from 'tailwind-merge';
+import SelectComponent from '@/components/common/select.component';
+
 export default function BorrowPage() {
   return (
-    <div className={twMerge('borrow-page-container', cssClass.borrowPage)}>Borrow content here</div>
+    <div className={twMerge('borrow-page-container', cssClass.borrowPage)}>
+      <div className="borrow-header">
+        Borrow overview
+        <SelectComponent />
+      </div>
+    </div>
   );
 }
 export const getStaticProps = async ({ locale }) => ({
