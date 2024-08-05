1aff248a12794a98e00d50b0822aaa8114882c95
dat <datvt@emurgo.africa>
2024-08-05 15:15:18 +0700

    update borrow flow

--- components/borrow/asset.component.tsx
+++ components/borrow/asset.component.tsx
@@ -1,12 +1,46 @@
 import cssClass from '@/components/borrow/asset.component.module.scss';
 import { twMerge } from 'tailwind-merge';
+import Image from 'next/image';
+import { Button } from 'antd';
 
 interface AssetProps {}
 
 export default function assetComponent(props: AssetProps) {
+  const handleAdjust = () => {};
+
   return (
     <div className={twMerge(cssClass.assetComponent)}>
-      <div className="asset-container">a</div>
+      <div className="asset-container">
+        <div className="asset-header">Asset to borrow</div>
+        <div className="">
+          <div className="gap-6 asset-nav">
+            <div className="basis-1/4">Asset</div>
+            <div className="basis-1/4">Loan Available</div>
+            <div className="basis-1/4">APR (variable)</div>
+            <div className="basis-1/4"></div>
+          </div>
+          <div className="gap-6 asset-body">
+            <div className="basis-1/4">
+              <Image
+                className="mr-2"
+                src="/images/borrow/tokens/usdc.png"
+                alt="USDC"
+                width={40}
+                height={40}
+              />
+              USDC
+            </div>
+            <div className="basis-1/4 flex-col items-start justify-center	">
+              <div>10,000.00</div>
+              <div className="usd">$ 4,000.00</div>
+            </div>
+            <div className="basis-1/4">0.07%</div>
+            <div className="basis-1/4  justify-end">
+              <Button onClick={() => handleAdjust}>Borrow</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- components/borrow/loans.component.tsx
+++ components/borrow/loans.component.tsx
@@ -1,6 +1,6 @@
 import cssClass from '@/components/borrow/loans.component.module.scss';
 import { twMerge } from 'tailwind-merge';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import { InfoCircleOutlined, CheckOutlined } from '@ant-design/icons';
 import { Tooltip } from 'antd';
 import React from 'react';
 import { Button } from 'antd';
@@ -13,7 +13,7 @@ export default function LoansComponent(props: LoansProps) {
     <div className={twMerge(cssClass.loansComponent)}>
       <div className="loans-container">
         <h3>My Loans</h3>
-        <div className="flex  px-4 py-2 gap-6">
+        <div className="flex px-4 py-2 gap-6 loans-header">
           <div className="basis-1/4  ">
             <h4>Asset</h4>
           </div>
@@ -32,24 +32,64 @@ export default function LoansComponent(props: LoansProps) {
             </h4>
           </div>
         </div>
-        <div className="px-4 py-4">
-          <div className="flex "></div>
-        </div>
-        <div className="flex ">
-          <div className="basis-1/2">Status: Active</div>
-          <div className="basis-1/2">Debt remain: 2,780.00 USDC $2,780.00</div>
-        </div>
-        <div className="flex ">
-          <div>Collateral: 2.5 WETH $6,540.00</div>
-          <div>Adjust collateral</div>
-        </div>
-        <div className="flex ">
-          <div>Yield-generating: Yield earned: 0.281 WETH</div>
-          <div>
-            <Button type="primary">Primary Button</Button>
-            <Button>Default Button</Button>
+        <div className="px-4 py-1 loans-inner">
+          <div className="flex gap-6">
+            <div className="flex items-center basis-1/4">
+              <Image
+                className="mr-2"
+                src="/images/borrow/tokens/usdc.png"
+                alt="USDC"
+                width={40}
+                height={40}
+              />
+              USDC
+            </div>
+            <div className="loans-size basis-1/4 ">
+              <h5>3,000.00</h5>
+              <div className="usd">$ 3,000.00</div>
+            </div>
+            <div className="loans-apr basis-1/4 justify-center flex items-center">1.82%</div>
+            <div className="loans-health basis-1/4 flex items-center justify-end">12.76</div>
+          </div>
+          <div className="flex justify-between loans-status gap-4">
+            <div className="">
+              Status: <span className="ml-1">Active</span>
+            </div>
+            <div></div>
+            <div className="flex justify-end loans-remain">
+              <div className="">Debt remain:</div>
+              <div className="flex flex-wrap flex-1">
+                <div className="highlight ml-1">2,780.0 USDC</div>
+                <div className="ml-1">$ 2,780.0</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex loans-collateral justify-between">
+            <div className="flex">
+              <span className="mr-1">Collateral:</span>
+              2.5 WETH
+              <span className="ml-1">$6,540.00</span>
+            </div>
+            <Button>Adjust collateral</Button>
+          </div>
+          <div className="flex justify-between items-end">
+            <div className="loans-yield">
+              <div>
+                Yield-generating: <CheckOutlined className="ml-1" />
+              </div>
+              <div>
+                Yield earned: <span className="ml-1">0.281 WETH</span>
+              </div>
+            </div>
+            <div className="loans-button ">
+              <Button type="primary" className="mr-2">
+                Borrow More
+              </Button>
+              <Button>Repay</Button>
+            </div>
           </div>
         </div>
+        {/* <div className="loans-empty">There is no loan yet.</div> */}
       </div>
     </div>
   );
