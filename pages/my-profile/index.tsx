e4540b81a9ed9df0afef84246f9f88eb0a8f6bf5
Thanh Ky <thanhky@192.168.1.3>
2024-08-28 20:59:19 +0700

    update profile layout

--- pages/my-profile/index.tsx
+++ pages/my-profile/index.tsx
@@ -6,6 +6,11 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { twMerge } from 'tailwind-merge';
 import { useAccount } from 'wagmi';
+import { EditOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { ScanIcon } from '@/components/icons/scan.icon'
+import { UserSquareIcon } from '@/components/icons/user-square.icon'
+import Image from 'next/image';
+
 export default function MyProfilePage() {
   /**
    * HOOKS
@@ -37,7 +42,6 @@ export default function MyProfilePage() {
   };
   return (
     <div className={twMerge('my-profile-page-container', cssClass.myProfilePage)}>
-      My profile page here
       <div className="flex items-center gap-8">
         <Button disabled={auth?.email} className="btn-primary-custom" onClick={handleUpdateAuth}>
           Sign in
@@ -66,6 +70,185 @@ export default function MyProfilePage() {
         <Button className="btn-primary-custom" onClick={() => openSignUpSuccessModal()}>
           Open signup success modal
         </Button>
+      </div>
+      <div className='my-profile-page-wrapper'>
+        <div className='my-profile-page-wrapper__title'>My Profile</div>
+        <div className='my-profile-page-wrapper__borrowed'>
+          <div className='my-profile-page-wrapper__borrowed__left'>
+            <div className='my-profile-page-wrapper__borrowed__item'>
+              <div className='my-profile-page-wrapper__borrowed__item__title'>
+                Borrowed balance
+              </div>
+              <div className='my-profile-page-wrapper__borrowed__item__value'>
+                <span className='my-profile-page-wrapper__borrowed__item__value__unit'>$</span>
+                1,875.00
+              </div>
+            </div>
+            <div className='my-profile-page-wrapper__borrowed__item'>
+              <div className='my-profile-page-wrapper__borrowed__item__title'>
+                Collateral
+              </div>
+              <div className='my-profile-page-wrapper__borrowed__item__value'>
+                <span className='my-profile-page-wrapper__borrowed__item__value__unit'>$</span>
+                4,017.87
+              </div>
+            </div>
+          </div>
+          <div className='my-profile-page-wrapper__borrowed__right'>
+            <div className='my-profile-page-wrapper__borrowed__item'>
+              <div className='my-profile-page-wrapper__borrowed__item__title'>
+                Net APY
+              </div>
+              <div className='my-profile-page-wrapper__borrowed__item__value'>
+                0.07
+                <span className='my-profile-page-wrapper__borrowed__item__value__unit'>%</span>
+              </div>
+            </div>
+            <div className='my-profile-page-wrapper__borrowed__item'>
+              <div className='my-profile-page-wrapper__borrowed__item__title'>
+                Finance health
+              </div>
+              <div className='my-profile-page-wrapper__borrowed__item__value'>
+                <span className='my-profile-page-wrapper__borrowed__item__value__health--good'>1.66B</span>
+                <InfoCircleOutlined size={12} style={{
+                  color: "#1890FF"
+                }} />
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="my-profile-page-wrapper__supply">
+          <div className="my-profile-page-wrapper__supply__item">
+            <span className="my-profile-page-wrapper__supply__item__label">
+              {t('SUPPLY_OVERVIEW_TOTAL_SUPPLY')}
+            </span>
+            <div className="my-profile-page-wrapper__supply__item__value">
+              ${' '}
+              <span
+                className="font-bold"
+                style={{
+                  color: '#F0F0F0',
+                }}>
+                4,567.87
+              </span>
+            </div>
+          </div>
+          <div className="my-profile-page-wrapper__supply__item">
+            <span className="my-profile-page-wrapper__supply__item__label">
+              {t('SUPPLY_OVERVIEW_NET_APY')}
+            </span>
+            <div className="my-profile-page-wrapper__supply__item__value">
+              <span
+                className="font-bold"
+                style={{
+                  color: '#F0F0F0',
+                }}>
+                0.07
+              </span>{' '}
+              %
+            </div>
+          </div>
+          <div className="my-profile-page-wrapper__supply__item">
+            <span className="my-profile-page-wrapper__supply__item__label">
+              {t('SUPPLY_OVERVIEW_TOTAL_EARNED')}
+            </span>
+            <div className="my-profile-page-wrapper__supply__item__value">
+              <span
+                className="font-bold"
+                style={{
+                  color: '#52C41A',
+                }}>
+                +$65.87
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='my-profile-page-wrapper__account'>
+        <div className='my-profile-page-wrapper__account__title'>
+          My Account
+        </div>
+        <div className='my-profile-page-wrapper__account__content'>
+          {/* <div className='my-profile-page-wrapper__account__content--no-account'>
+            <Button
+              type="primary"
+              className={twMerge('btn-primary-custom', 'my-profile-page-wrapper__account__content--no-account__signup')}
+            >
+              Signup for an account
+            </Button>
+            <div className='my-profile-page-wrapper__account__content--no-account__signin'>
+              Already had an account?
+              <Button type='link'>Signin</Button>
+            </div>
+          </div> */}
+          <div className='my-profile-page-wrapper__account__content--has-account--no-verified'>
+            <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content'>
+              <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__title'>
+                Account detail
+              </div>
+              <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body'>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item'>
+                  <span className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__title'>
+                    Username
+                  </span>
+                  <span className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__value'>username01</span>
+                </div>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item'>
+                  <span className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__title'>
+                    Email
+                  </span>
+                  <span className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__value'>mail@mail.com</span>
+                </div>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item'>
+                  <span className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__title'>
+                    Password
+                  </span>
+                  <div className='my-profile-page-wrapper__account__content--has-account--no-verified__content__body__item__value'>
+                    ********  <Button icon={<EditOutlined />} type="text">
+                      Search
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer'>
+              <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left'>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item'>
+                  <UserSquareIcon className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__icon' />
+                  <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__wrapper'>
+                    <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__title'>
+                      Step 1
+                    </div>
+                    Provide identity document
+                  </div>
+                </div>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item'>
+                  <ScanIcon className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__icon' />
+                  <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__wrapper'>
+                    <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__left__item__title'>
+                      Step 2
+                    </div>
+                    Get ready for a face scan
+                  </div>
+                </div>
+              </div>
+
+              <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__right'>
+                <Button
+                  type="primary"
+                  htmlType='submit'
+                  className={twMerge('btn-primary-custom')}
+                >
+                  Continue
+                </Button>
+                <div className='my-profile-page-wrapper__account__content--has-account--no-verified__footer__right__powered'>
+                  powered by <Image src="/images/sumsub.png" alt="Sumsub" width={84} height={34} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
