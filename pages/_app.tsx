7c3d6b20e57baacc2f302bf89414cc4cc73b51c8
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-09-26 23:47:50 +0700

    feat: update footer new layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -104,6 +104,10 @@ function App({ Component, ...rest }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Provider store={store}>
