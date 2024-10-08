2462c7449cde09ba05bbb2c7d453d822e09268d5
dat <datvt@emurgo.africa>
2024-10-08 15:08:06 +0700

    update UX

--- components/borrow/modal-borrow.component.tsx
+++ components/borrow/modal-borrow.component.tsx
@@ -164,11 +164,14 @@ export default function ModalBorrowComponent({
           setStatus(TRANSACTION_STATUS.SUCCESS);
         }
         if (tx?.error) {
+          setStep(2);
           setStatus(TRANSACTION_STATUS.FAILED);
           setErrorTx(tx.error as any);
         }
         setLoading(false);
       } catch (error) {
+        setStep(2);
+        setErrorTx(error);
         setStatus(TRANSACTION_STATUS.FAILED);
         setLoading(false);
       }
@@ -214,11 +217,14 @@ export default function ModalBorrowComponent({
           setStatus(TRANSACTION_STATUS.SUCCESS);
         }
         if (tx?.error) {
+          setStep(2);
           setStatus(TRANSACTION_STATUS.FAILED);
           setErrorTx(tx.error as any);
         }
         setLoading(false);
       } catch (error) {
+        setStep(2);
+        setErrorTx(error);
         setStatus(TRANSACTION_STATUS.FAILED);
         setLoading(false);
       }
