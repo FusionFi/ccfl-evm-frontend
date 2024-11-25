ebc43dcce4156cc0345500f3849f91b06b4328c9
rhystmorgan <morgan.rhystomas@gmail.com>
2024-11-25 10:28:07 +0000

    added fusion infrastructure wallet

--- utils/cardano/owner.ts
+++ utils/cardano/owner.ts
@@ -1,13 +1,13 @@
 // import { initLucid } from "./blockfrost"
 
-export const ownerPKH = "3f765f0eeb5b081a764f10efb376d14aaa1a4d37a7853e4a98b987a0"
+export const ownerPKH = "efef78473ad4a824c82e7bb372ac4d59c1213815ca6d53103b4ae97c"
 
 // export const ownerPKH = lucid.utils.getAddressDetails(process.env.OWNER_ADDR)
 //   .paymentCredential!.hash;
 
-export const ownerAddress = "addr_test1vqlhvhcwaddssxnkfugwlvmk69925xjdx7nc20j2nzuc0gq43pzgq";
+export const ownerAddress = "addr_test1vrh777z88t22sfxg9eamxu4vf4vuzgfczh9x65cs8d9wjlq9xrqwf";
 
-export const ownerSKey = "ed25519_sk16dqkksjz64sevys5q8glcj0h3xjrfezgkzd5yzzr6selghu3nrzqfv809v";
+export const ownerSKey = "ed25519_sk1c2hgyeufc4k43acch8sq0q7klc05s5h0rdr7zx6taugwynf65n7sy37839";
 
 // export const interestPayAddr = "addr_test1vr4m7cd94yhymrwcmgs2k6zs00jql9d075ms0dgxjv2tuxqjy82wz"
 
